0232101d096d2f86e6e620e682ad726b1efab1df
Aswini <aswini.skysea@gmail.com>
2024-08-22 18:57:05 +0530

    config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/aswini-skysea/AviaxMusic",
+    "https://github.com/aswini-skysea/HB-Live",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
